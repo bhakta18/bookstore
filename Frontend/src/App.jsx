2f8747409445226bc59258ff276eb00c55fd6f89
Bhakta <bhaktabhoi.in@gmail.com>
2024-11-02 00:44:43 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Home from './home/Home'
+import Course from './components/Course'
+import { Route, Routes } from "react-router-dom"
+
+
+function App() {
+  return (<>
+  {/* <Home/>
+   <Course/>*/}
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/Course" element={<Course/>}/>
+   </Routes>
+  </>)
+  
+}
+
+export default App
